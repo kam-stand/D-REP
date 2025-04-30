86900ed278016252ca2947083958fd0001bcb908
kamstand <kamrul.hassan@stonybrook.edu>
2025-04-30 06:27:30 -0400

    added init cache function

--- src/init.d
+++ src/init.d
@@ -3,5 +3,11 @@ module init;
 
 import core.stdc.stdio;
 import core.stdc.stdlib;
+import core.sys.posix.stdio;
 
 
+int init_cache(void)
+{
+    int status = system("find ~/ -path '*/.git' -prune -o -print > ./cache.txt");
+    return status;
+}
