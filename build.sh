076307732b40410a83ca0cfb4cc73c0a835c3092
kamstand <kamrul.hassan@stonybrook.edu>
2025-04-30 00:46:15 -0400

    removed cache files

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 
 echo "building exectuable... "
 
+echo "cache cmd 'find ~/ -path '*/.git' -prune -o -print > cache.txt'"
 ldc2 -w -vgc -betterC ./src/cmd.d ./src/main.d ./src/query.d -of=bin/d-grep 
 
